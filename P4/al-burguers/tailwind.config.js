c7f9a73f25559ba6f9ab3a72858ee20ded3ac925
xGeaa <alejandrogea2004@gmail.com>
2026-05-13 18:38:00 +0200

    Arreglado proyecto react

---
+++ P4/al-burguers/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
+  ],
+  prefix: "",
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+}
